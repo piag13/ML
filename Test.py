8f83d88996f37e3f1b2896de3ea3a967e0f86d59
piag13 <145634829+piag13@users.noreply.github.com>
2025-06-13 14:14:26 +0700

    tăng cường dữ liệu để làm giảm sự mất cân bằng commit lần 1 vì file quá lớn

---
+++ Test.py
@@ -0,0 +1,13 @@
+from collections import Counter
+import os
+
+label_dir = 'dataset_balanced/train_data/labels/val'
+class_counts = Counter()
+
+for label_file in os.listdir(label_dir):
+    with open(os.path.join(label_dir, label_file)) as f:
+        for line in f:
+            class_id = line.strip().split()[0]
+            class_counts[class_id] += 1
+
+print(class_counts)
